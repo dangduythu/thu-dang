6af2625bba8da16bdc9bd30050469d6d68a0c45d
dangduythu <82801103+dangduythu@users.noreply.github.com>
2026-09-26 15:37:27 +0700

    Configure English Learning 11.0 index.js

--- EnglishLearning_10_5_APK_Ready/index.js
+++ EnglishLearning_10_5_APK_Ready/index.js
@@ -1,8 +1,12 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+function RootApp() { return <SafeAreaProvider><StatusBar style="dark" /><App /></SafeAreaProvider>; }
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(RootApp);
